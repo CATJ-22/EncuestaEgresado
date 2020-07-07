00db390fdf4749f98a89aab1875069cf93822cb8
Kiriam02 <59971744+Kiriam02@users.noreply.github.com>
2020-07-07 15:49:13 -0500

    imagenes
    Creacion de la carpeta imagenes y se agregó las imagenes para trbajar en el trabajo.

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,18 +1,24 @@
-@extends('layouts.app')
-@section('content')
-<form action="encuesta" method="post">
-        @csrf 
-        <div class="row">
-            @if($errors->any())
-            <div class="alert alert-error">
-                <ul>
-                    @foreach($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<div class="sidenav">
+         <div class="login-main-text">
+            <h2>INICIO DE SESIÓN</h2>
+         </div>
+      </div>
+      <div class="main">
+         <div class="col-md-6 col-sm-12">
+            <div class="login-form">
+               <form>
+                  <div class="form-group">
+                     <label>Cédula</label>
+                     <input type="text" class="form-control" placeholder="Ingrese su cédula">
+                  </div>
+                  <div class="form-group">
+                     <label>Contraseña</label>
+                     <input type="password" class="form-control" placeholder="************">
+                  </div>
+                  <button type="submit" class="btn btn-black">Acceder</button>
+               </form>
             </div>
-            @endif
-            <div id="renderArea" class="w-100"></div>
-        </div>  
-</form>
-@endsection
+         </div>
+      </div>
